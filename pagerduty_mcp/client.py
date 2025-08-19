5904124ec1de7717eac7a3c059428a8f1bcf8e01
Jose Corte-Real <jcorte-real@pagerduty.com>
2025-08-19 20:45:00 +0100

    feat: added support for injecting a new client using contextvars

--- pagerduty_mcp/client.py
+++ pagerduty_mcp/client.py
@@ -1,12 +1,12 @@
 import logging
 import os
+from collections.abc import Callable
 from contextvars import ContextVar
 from functools import lru_cache
 from importlib import metadata
 
-import pagerduty
 from dotenv import load_dotenv
-from pydantic import BaseModel
+from pagerduty.rest_api_v2_client import RestApiV2Client
 
 from pagerduty_mcp import DIST_NAME
 
@@ -19,27 +19,23 @@ API_KEY = os.getenv("PAGERDUTY_USER_API_KEY")
 API_HOST = os.getenv("PAGERDUTY_API_HOST", "https://api.pagerduty.com")
 
 
-class PagerdutyMCPClient(pagerduty.RestApiV2Client):
+class PagerdutyMCPClient(RestApiV2Client):
     @property
     def user_agent(self) -> str:
         return f"{DIST_NAME}/{metadata.version(DIST_NAME)} {super().user_agent}"
 
 
-class ClientConfig(BaseModel):
-    api_key: str
-    api_host: str = "https://api.pagerduty.com"
-
-
-pd_client_config: ContextVar[ClientConfig | None] = ContextVar("pd_client_client", default=None)
+ClientFactory = Callable[[], RestApiV2Client]
+pd_client_factory: ContextVar[ClientFactory | None] = ContextVar("pd_client_factory", default=None)
 
 
 @lru_cache(maxsize=1)
-def _get_cached_client(api_key: str, api_host: str) -> pagerduty.RestApiV2Client:
+def _get_cached_client(api_key: str, api_host: str) -> RestApiV2Client:
     """Get a cached PagerDuty client."""
     return create_pd_client(api_key, api_host)
 
 
-def create_pd_client(api_key: str, api_host: str | None = None) -> pagerduty.RestApiV2Client:
+def create_pd_client(api_key: str, api_host: str | None = None) -> RestApiV2Client:
     """Get the PagerDuty client."""
     pd_client = PagerdutyMCPClient(api_key)
     if api_host:
@@ -48,7 +44,7 @@ def create_pd_client(api_key: str, api_host: str | None = None) -> pagerduty.Res
     return pd_client
 
 
-def get_client() -> pagerduty.RestApiV2Client:
+def get_client() -> RestApiV2Client:
     """Get the PagerDuty client, using cached configuration if available.
 
     This function will check if client config information is stored in a context var.
@@ -56,12 +52,8 @@ def get_client() -> pagerduty.RestApiV2Client:
     we need to update the client credentials for each request, since remote MCP servers
     need to support multi tenancy.
     """
-    client = _get_cached_client(API_KEY, API_HOST)
-    client_config = pd_client_config.get()
-    if client_config is None:
-        logger.info("Using default PagerDuty client configuration")
-        return client
-    client.api_key = client_config.api_key
-    client.url = client_config.api_host
+    client_factory = pd_client_factory.get(None)
+    if client_factory is not None:
+        return client_factory()
 
-    return client
+    return _get_cached_client(API_KEY, API_HOST)
